9fdd65c050fc170c90f5364867a80c0d3922293a
lorenz <lorencecolare10@gmail.com>
2024-07-03 20:17:06 +0800

    .

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def get_line_numbers():
 
 # Initialize the main window
 root = tk.Tk()
-root.title("Text Editor")
+root.title("B++ Junior Professional File Editor")
 
 # Set the window icon
 icon_path = os.path.join(basedir, "icons", "favicon.ico")
